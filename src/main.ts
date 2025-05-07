6f5c015ad4ddd3da2f4db7b7cc4442614ab69ba4
João Victor <vektor5665@hotmail.com>
2025-05-07 00:08:50 -0300

    inclusão do CORS

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors(); // se for necessário rodar frontend separado
+
+  
+  app.enableCors();
+
+  
   await app.listen(3000);
 }
 bootstrap();

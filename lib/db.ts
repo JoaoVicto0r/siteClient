393a5a435c07dfe9d763eac8d9bcb68f9004f6a6
João Victor <vektor5665@hotmail.com>
2025-05-09 12:28:49 -0300

    atualização 1.0

--- lib/db.ts
+++ lib/db.ts
@@ -1,11 +1,12 @@
 import { PrismaClient } from "@prisma/client"
 
-declare global {
-  var prisma: PrismaClient | undefined
-}
+// Evita múltiplas instâncias do Prisma Client em desenvolvimento
+const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
-export const db = globalThis.prisma || new PrismaClient()
+export const db =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+  })
 
-if (process.env.NODE_ENV !== "production") {
-  globalThis.prisma = db
-}
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db
